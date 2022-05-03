3d9c4a296f7a7f387c5f7746a89dcab862026a87
Dejan Trajkovic <dtrajko@gmail.com>
2022-05-03 14:36:54 +0200

    2. Octrees / Splitting a Node

--- Assets/Octree/OctreeNode.cs
+++ Assets/Octree/OctreeNode.cs
@@ -26,11 +26,29 @@ public class OctreeNode
     {
         nodeBounds = b;
         minSize = minNodeSize;
+
+        float quarter = nodeBounds.size.y / 4f;
+        float childLength = nodeBounds.size.y / 2f;
+        Vector3 childSize = new Vector3(childLength, childLength, childLength);
+        childBounds = new Bounds[8];
+        childBounds[0] = new Bounds(nodeBounds.center + new Vector3(-quarter,  quarter, -quarter), childSize);
+        childBounds[1] = new Bounds(nodeBounds.center + new Vector3( quarter,  quarter, -quarter), childSize);
+        childBounds[2] = new Bounds(nodeBounds.center + new Vector3(-quarter,  quarter,  quarter), childSize);
+        childBounds[3] = new Bounds(nodeBounds.center + new Vector3( quarter,  quarter,  quarter), childSize);
+        childBounds[4] = new Bounds(nodeBounds.center + new Vector3(-quarter, -quarter, -quarter), childSize);
+        childBounds[5] = new Bounds(nodeBounds.center + new Vector3( quarter, -quarter, -quarter), childSize);
+        childBounds[6] = new Bounds(nodeBounds.center + new Vector3(-quarter, -quarter,  quarter), childSize);
+        childBounds[7] = new Bounds(nodeBounds.center + new Vector3( quarter, -quarter,  quarter), childSize);
     }
 
     public void Draw()
     {
         Gizmos.color = new Color(0, 1, 0);
         Gizmos.DrawWireCube(nodeBounds.center, nodeBounds.size);
+
+        foreach (Bounds childBound in childBounds)
+        {
+            Gizmos.DrawWireCube(childBound.center, childBound.size);
+        }
     }
 }

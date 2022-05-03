5d11a34ac2a09f9974c5d3b32a17b63a148d6c1c
Dejan Trajkovic <dtrajko@gmail.com>
2022-05-03 13:17:02 +0200

    2. Octrees / The Octree Base Classes

--- Assets/CreateOctree.cs
+++ Assets/CreateOctree.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CreateOctree : MonoBehaviour
 {
+    public GameObject[] worldObjects;
+    Octree ot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ot = new Octree(worldObjects, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            Gizmos.color = new Color(0, 1, 0);
+            Gizmos.DrawWireCube(ot.boundsD.center, ot.boundsD.size);
+        }
     }
 }
--- Assets/Octree/OctreeNode.cs
+++ Assets/Octree/OctreeNode.cs
@@ -5,6 +5,4 @@ using UnityEngine;
 public class OctreeNode
 {
 
-    
-
 }

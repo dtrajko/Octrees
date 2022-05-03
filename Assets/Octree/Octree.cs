fa2138b705e3752d62801bf7ed12004d0b328fe8
Dejan Trajkovic <dtrajko@gmail.com>
2022-05-03 12:24:15 +0200

    2. Octrees / The Octree Base Classes (1)

---
+++ Assets/Octree/Octree.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Octree
+{
+    public OctreeNode rootNode;
+    public Bounds boundsD;
+
+    public Octree(GameObject[] worldObjects, float minNodeSize)
+    {
+        Bounds bounds = new Bounds();
+
+        foreach (GameObject go in worldObjects)
+        {
+            bounds.Encapsulate(go.GetComponent<Collider>().bounds);
+        }
+
+        boundsD = bounds;
+    }
+
+}

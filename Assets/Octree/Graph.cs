6ee2c04c97d219adcc1b07bccd44dfed1be144e3
Dejan Trajkovic <dtrajko@gmail.com>
2022-05-05 01:49:19 +0200

    4. Pathfinding / Implementing A*

--- Assets/Octree/Graph.cs
+++ Assets/Octree/Graph.cs
@@ -79,4 +79,116 @@ public class Graph
         }
     }
 
+    public bool AStar(OctreeNode startNode, OctreeNode endNode)
+    {
+        pathList.Clear();
+
+        Node start = findNode(startNode.id);
+        Node end = findNode(endNode.id);
+
+        if (start == null || end == null)
+        {
+            return false;
+        }
+
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+
+        float tentative_g_score = 0;
+        bool tentative_is_better;
+
+        start.g = 0;
+        start.h = Vector3.SqrMagnitude(startNode.nodeBounds.center - endNode.nodeBounds.center);
+        start.f = start.h;
+
+        open.Add(start);
+
+        while (open.Count > 0)
+        {
+            int i = lowestF(open);
+            Node thisnode = open[i];
+            if (thisnode.octreeNode.id == endNode.id)
+            {
+                reconstructPath(start, end);
+                return true;
+            }
+            open.RemoveAt(i);
+            closed.Add(thisnode);
+
+            Node neighbour;
+            foreach (Edge e in thisnode.edgeList)
+            {
+                neighbour = e.endNode;
+                neighbour.g = thisnode.g + Vector3.SqrMagnitude(
+                    thisnode.octreeNode.nodeBounds.center - neighbour.octreeNode.nodeBounds.center);
+                if (closed.IndexOf(neighbour) > -1)
+                {
+                    continue;
+                }
+
+                tentative_g_score = thisnode.g + Vector3.SqrMagnitude(
+                    thisnode.octreeNode.nodeBounds.center - neighbour.octreeNode.nodeBounds.center);
+                if (open.IndexOf(neighbour) == -1)
+                {
+                    open.Add(neighbour);
+                    tentative_is_better = true;
+                }
+                else if (tentative_g_score < neighbour.g)
+                {
+                    tentative_is_better = true;
+                }
+                else
+                {
+                    tentative_is_better = false;
+                }
+
+                if (tentative_is_better)
+                {
+                    neighbour.cameFrom = thisnode;
+                    neighbour.g = tentative_g_score;
+                    neighbour.h = Vector3.SqrMagnitude(
+                        thisnode.octreeNode.nodeBounds.center - endNode.nodeBounds.center);
+                    neighbour.f = neighbour.g + neighbour.h;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void reconstructPath(Node startId, Node endId)
+    {
+        pathList.Clear();
+        pathList.Add(endId);
+        var p = endId.cameFrom;
+        while (p != startId && p != null)
+        {
+            pathList.Insert(0, p);
+            p = p.cameFrom;
+        }
+        pathList.Insert(0, startId);
+    }
+
+    int lowestF(List<Node> l)
+    {
+        float lowestf = 0;
+        int count = 0;
+        int iteratorCount = 0;
+
+        for (int i = 0; i < l.Count; i++)
+        {
+            if (i == 0)
+            {
+                lowestf = l[i].f;
+                iteratorCount = count;
+            }
+            else if (l[i].f <= lowestf)
+            {
+                lowestf = l[i].f;
+                iteratorCount = count;
+            }
+            count++;
+        }
+        return iteratorCount;
+    }
+
 }
--- Assets/Octree/Node.cs
+++ Assets/Octree/Node.cs
@@ -5,6 +5,10 @@ public class Node
     public List<Edge> edgeList = new List<Edge>();
     public Node path = null;
     public OctreeNode octreeNode;
+    public float g; // movement cost
+    public float h; // heuristic (best intelligent guess)
+    public float f; // h + g
+    public Node cameFrom;
 
     // Update is called once per frame
     public Node(OctreeNode n)
